1f492a7190e656da2b18e2c6393acb46de84bf60
Sarvesh521 <122361969+Sarvesh521@users.noreply.github.com>
2024-09-21 02:50:31 +0530

    initial commits
    Day 1

---
+++ thresholds.py
@@ -0,0 +1,76 @@
+
+
+# Get thresholds for beginner mode
+def get_thresholds_beginner():
+
+    _ANGLE_HIP_KNEE_VERT = {
+                            'NORMAL' : (0,  32),
+                            'TRANS'  : (35, 65),
+                            'PASS'   : (70, 95)
+                           }
+    
+    _ANGLE_ELBOW_WRIST_VERT = {
+                            'NORMAL' : (180,  121),
+                            'TRANS'  : (120, 36),
+                            'PASS'   : (35, 0)
+    }
+
+        
+    thresholds = {
+    "SQUAT": {
+                    'HIP_KNEE_VERT': _ANGLE_HIP_KNEE_VERT,
+
+                    'HIP_THRESH'   : [10, 50],
+                    'ANKLE_THRESH' : 45,
+                    'KNEE_THRESH'  : [50, 70, 95],
+
+                    'OFFSET_THRESH'    : 35.0,
+                    'INACTIVE_THRESH'  : 15.0,
+
+                    'CNT_FRAME_THRESH' : 50}
+    ,
+    "CURL" : {
+                        'ELBOW_WRIST_VERT': _ANGLE_ELBOW_WRIST_VERT,
+
+                        'SHOULDER_THRESH'   : 130,
+                        # 'WRIST_THRESH'   : 45,
+                        # 'ELBOW_THRESH': [50, 70, 95],
+                        'ELBOW_THRESH': [150, 0, -20],
+
+                        'OFFSET_THRESH'    : 35.0,
+                        'INACTIVE_THRESH'  : 15.0,
+
+                        'CNT_FRAME_THRESH' : 50
+                            
+                    }
+    }
+
+    return thresholds
+
+
+
+# Get thresholds for pro mode
+def get_thresholds_pro():
+
+    _ANGLE_HIP_KNEE_VERT = {
+                            'NORMAL' : (0,  32),
+                            'TRANS'  : (35, 65),
+                            'PASS'   : (80, 95)
+                           }    
+
+        
+    thresholds = {
+                    'HIP_KNEE_VERT': _ANGLE_HIP_KNEE_VERT,
+
+                    'HIP_THRESH'   : [15, 50],
+                    'ANKLE_THRESH' : 30,
+                    'KNEE_THRESH'  : [50, 80, 95],
+
+                    'OFFSET_THRESH'    : 35.0,
+                    'INACTIVE_THRESH'  : 15.0,
+
+                    'CNT_FRAME_THRESH' : 50
+                            
+                 }
+                 
+    return thresholds
