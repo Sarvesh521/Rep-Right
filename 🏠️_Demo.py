f8f941148f4c0d71b4473bbd4319b8d99e7432fd
dh-r-uv <Dhruv.Kothari@iiitb.ac.in>
2024-09-22 20:37:17 +0530

    RepRight

--- 🏠️_Demo.py
+++ 🏠️_Demo.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title('AI Fitness Trainer: Squats Analysis')
+st.title('RepRight: Exercis eAnalysis')
 recorded_file = 'output_sample.mp4'
 sample_vid = st.empty()
 sample_vid.video(recorded_file)
