dcf1bafa092a75e81d745b1ba971c8585784bc74
Sarvesh521 <122361969+Sarvesh521@users.noreply.github.com>
2024-09-22 15:28:09 +0530

    audio integrated

--- Classifier.py
+++ Classifier.py
@@ -4,6 +4,8 @@ import numpy as np
 from PIL import Image
 import cv2
 from transformers import AutoImageProcessor, AutoModelForImageClassification
+import pyttsx3
+from moviepy.editor import VideoFileClip
 
 class_names = ['bicep_curl', 'pushup', 'shoulder press', 'squat']
 
@@ -19,13 +21,26 @@ def preprocess_frame(frame, target_size=(224, 224)):
     img_tensor = img_tensor.unsqueeze(0) 
     return img_tensor
 
-def predict_video(video_path):
-    cap = cv2.VideoCapture(video_path)
+def predict_video(video_path=None):
+    if video_path is None:
+        cap = cv2.VideoCapture(0)
+    else:
+        cap = cv2.VideoCapture(video_path)
     if not cap.isOpened():
         print("Error: Could not open video.")
         return
     frame_count = 0
+    audio_output=[]
+    flag=0
     while cap.isOpened():
+        if flag==0 and len(audio_output)>50:
+            engine = pyttsx3.init()
+            engine.setProperty('rate', 150)
+            engine.setProperty('volume', 1)
+            text = f"{max((audio_output), key = audio_output.count)} Exercise Detected"
+            engine.say(text)
+            engine.runAndWait()
+            flag=1
         ret, frame = cap.read()
         if ret:
             frame_count += 1
@@ -38,6 +53,7 @@ def predict_video(video_path):
             
             prediction_text = f"{class_names[predicted_class.item()]} ({probabilities[0][predicted_class.item()]:.2f})"
             print(f"Frame {frame_count}: {prediction_text}")
+            audio_output.append(class_names[predicted_class.item()])
             
             # Write the prediction onto the frame
             cv2.putText(frame, prediction_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
@@ -49,6 +65,12 @@ def predict_video(video_path):
             break
     cap.release()
     cv2.destroyAllWindows()
+    if flag==0:
+        engine = pyttsx3.init()
+        engine.setProperty('rate', 150)
+        engine.setProperty('volume', 1)
+        text = f"{max(set(audio_output), key = audio_output.count)} Exercise Detected"
+        engine.say(text)
+        engine.runAndWait()
 
-video_path = './Model_Classify_Test/pushupv1.mp4' 
-predict_video(video_path)
+predict_video('./Model_Classify_Test/pushupv1.mp4')
